a3ee92a5acb3662af785dec8666d91f026db28b9
Ithus <>
2020-11-04 12:12:38 +0700

    Update Test Application

--- Test/hello-world-1/src/app.py
+++ Test/hello-world-1/src/app.py
@@ -13,7 +13,26 @@ def hello():
 
     return render_template('index.html', message=message)
 
+from google.cloud import speech_v1 as speech
+
+def speech_to_text(config, audio):
+    client = speech.SpeechClient()
+    response = client.recognize(config=config, audio=audio)
+    print_sentences(response)
+
+def print_sentences(response):
+    for result in response.results:
+        best_alternative = result.alternatives[0]
+        transcript = best_alternative.transcript
+        confidence = best_alternative.confidence
+        print('-' * 80)
+        print(f'Transcript: {transcript}')
+        print(f'Confidence: {confidence:.0%}')
+        
+config = {'language_code': 'en-US'}
+audio = {'uri': 'gs://cloud-samples-data/speech/brooklyn_bridge.flac'}
+
 if __name__ == '__main__':
-    
+    speech_to_text(config, audio)
     '''server_port = os.environ.get('PORT', '8080')
     app.run(debug=False, port=server_port, host='0.0.0.0')'''
