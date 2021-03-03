91eca4745a8770c9baad54f23a1d91890742bb2f
Ithus <>
2021-03-03 11:45:29 +0700

    File selector update
    -The file selector is working now (just for wavf files with bit rate at 48000. 44100 won't work.)

--- Test/hello-world-1/src/main.py
+++ Test/hello-world-1/src/main.py
@@ -1,10 +1,14 @@
 from word_matching import wordMatcher_demo
+from word_matching import wordMatcher_demo_index
 import os
 
 print("\n\n"+"-"*10+"INITIALIZATION COMPLETED"+"-"*10+"\n")
 
+# show all wav files in resources
 print("Available Files:")
 wavFilesName = []
+wavSelectedFile = ''
+
 i=0
 for f in os.scandir(path="./resources"):
     if f.is_file():
@@ -14,6 +18,7 @@ for f in os.scandir(path="./resources"):
             print('\t'+str(i)+". "+f.name)
             i+=1
 
+# wav files selecor
 selection=True
 while(selection):   
     x = input("\nPlease enter the index of the file you want to work on... ")
@@ -22,8 +27,42 @@ while(selection):
         # print("DEBUG: '"+x+"' IS A DIGIT")
         if int(x)<i:
             print("SELECTED FILE")
-            print('\t'+str(x)+". {}".format(wavFilesName[int(x)]))
+            wavSelectedFile = wavFilesName[int(x)]
+            print('\t'+str(x)+". {}".format(wavSelectedFile))
             selection=False
+        else:
+            print("That number is not exist in the list above, please try again.")
+        
+    else:
+        # print("DEBUG: '"+x+"' IS A NOT DIGIT")
+        print("That's not a number, please enter a number of index.")
+
+# show all txt files in resources/script
+print("\nAvailable Scripts:")
+txtFilesName = []
+txtSelectedFile = ''
+
+i=0
+for f in os.scandir(path="./resources/script"):
+    if f.is_file():
+        (fname, ftype) = os.path.splitext(f)
+        if ftype == '.txt':
+            txtFilesName.append(f.name)
+            print('\t'+str(i)+". "+f.name)
+            i+=1
+
+# txt files selecor
+selection_script=True
+while(selection_script):   
+    x = input("\nPlease enter the index of the script you need... ")
+
+    if x.isdigit() == True:
+        # print("DEBUG: '"+x+"' IS A DIGIT")
+        if int(x)<i:
+            print("SELECTED FILE")
+            txtSelectedFile = txtFilesName[int(x)]
+            print('\t'+str(x)+". {}".format(txtSelectedFile))
+            selection_script=False
         else:
             print("That number is not exist in the list above, please try again.")
         
@@ -34,8 +73,10 @@ while(selection):
 
 print("\n\n------start------\n")
 
+result = wordMatcher_demo(wavSelectedFile,txtSelectedFile)
 print("result = "+str(
-    # wordMatcher_demo(4)
+    result
+    # wordMatcher_demo_index(4)
     # index
         # 0 = gsSample in English from google (not being used in Demo)
         # 1 = wavSample in English from google
@@ -45,6 +86,23 @@ print("result = "+str(
         # 5 = 04Noey.m4a
     ))
 
+# commenting (not necessary, just for fun)
+if result > 0.9 :
+    print("That's Great!")
+elif result > 0.8 :
+    print("Not bad though")
+elif result > 0.7 :
+    print("Hmm, could be better...")
+elif result > 0.6 :
+    print("Something is wrong I can feel it.")
+elif result > 0.5 :
+    print("Is the file match the script?")
+elif result > 0.4 :
+    print("Are you joking?")
+elif result > 0.3 :
+    print("Holy...")
+else:
+    print("...\nwat\nda\n...")
 
 
 print("\n\n-------end-------\n")
--- Test/hello-world-1/src/word_matching.py
+++ Test/hello-world-1/src/word_matching.py
@@ -11,8 +11,71 @@ from pydub import AudioSegment
 # pylint: disable=C0103
 app = Flask(__name__)
         
+def transcribe_file(filepath,scriptpath):
+    from google.cloud import speech
 
-def transcribe_file(filejson):
+    script = open(scriptpath,encoding='utf-8').read()
+    path = filepath
+    sampleRate = 48000
+    channel = 2
+    lang = "th-TH"
+
+    script_words=deepcut.tokenize(script)
+    script_words=spaceEliminator(script_words)
+
+    print("-"*10+"PLEASE WAIT"+"-"*10)
+
+    client = speech.SpeechClient()
+
+    with io.open(path, "rb") as audio_file:
+        content = audio_file.read()
+
+    audio = speech.RecognitionAudio(content=content)
+
+    config = speech.RecognitionConfig(
+        encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+        sample_rate_hertz=sampleRate,
+        language_code=lang,
+        audio_channel_count=channel,
+        enable_separate_recognition_per_channel=True,
+    )
+
+    response = client.recognize(config=config, audio=audio)
+
+    for i,result in enumerate(response.results):
+        alternative = result.alternatives[0]
+        transcript = alternative.transcript
+
+        transcript_words=deepcut.tokenize(transcript)
+        transcript_words=spaceEliminator(transcript_words)
+
+        print("-" * 20)
+        print("First alternative of result {}".format(i))
+        print(u"Transcript: {}".format(transcript))
+        print("Script: {}".format(script))
+        #print("Deepcut: {}".format(deepcut.tokenize(transcript)))
+        match = 0
+            
+            #choosing which listhas the lesser lenght...
+        if len(script_words) < len(transcript_words):
+            shorterScriptCount = len(script_words)
+        else:
+            shorterScriptCount = len(transcript_words)
+
+            # ...and use it in matching.
+        for i in range(shorterScriptCount):
+            # print("\tDEBUG: MATCHING WORD:( "+transcript_words[i]+" , "+script_words[i]+" )") # DEBUG
+            if script_words[i] == transcript_words[i]:
+                # print("\tDEBUG: MATCHING RESULT: MATCHED")        # DEBUG
+                match+=1
+            # else:
+                # print("\tDEBUG: MATCHING RESULT: NOT MATCHED")    # DEBUG
+        match_result=match/len(script_words)
+        print("Matched Result: {} %".format(match_result*100))      # show match result in percentage.
+        print(u"Channel Tag: {}".format(result.channel_tag))        
+    return match_result
+
+def transcribe_file_fromJson(filejson):
     from google.cloud import speech
 
     # script = filejson["script"]
@@ -133,7 +196,14 @@ def transcribe_gcs(filejson):
         print(u"Channel Tag: {}".format(result.channel_tag))
     return match_result
 
-def wordMatcher_demo(index):
+def wordMatcher_demo_index(index):
+    # index
+        # 0 = gsSample in English from google (not being used in Demo)
+        # 1 = wavSample in English from google
+        # 2 = wavSample in English
+        # 3 = wavSample in Thai
+        # 4 = 04Noey.wav
+        # 5 = 04Noey.m4a
 
     with open('src/data.json', encoding="utf-8") as json_file:
         data = json.load(json_file)
@@ -148,6 +218,15 @@ def wordMatcher_demo(index):
     else:
         print("Transcribing File '"+filejson['name']+"' on path: "+filejson['path'])
         return transcribe_file(filejson)
+
+def wordMatcher_demo(filename,scriptname):
+
+    filepath = "resources/"+filename
+    scriptpath = "resources/script/"+scriptname
+
+    print("Transcribing File '"+filename+"' on path: "+filepath)
+    return transcribe_file(filepath,scriptpath)
+    # return 1
 
 def spaceEliminator(list):
     poppingIndexes = []
